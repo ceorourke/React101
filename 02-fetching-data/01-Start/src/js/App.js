04158f7e8303f87fbff4a2f94d0e28a0370a216f
colleen <elena.colleen@gmail.com>
2018-01-25 15:21:14 -0800

    using fetch and .then to parse data from external API

---
+++ 02-fetching-data/01-Start/src/js/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import image from '../images/cloud-upload-download-data-transfer.svg';
+import Collapsible from './Collapsible';
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            contacts: []
+        }
+    }
+
+    componentDidMount() {
+        // fetch data from external API
+        this.fetchData();
+    }
+
+    fetchData() {
+        // call to eternal API for random user data
+        fetch('https://randomuser.me/api/?results=50&nat=us,dk,fr,gb')
+        .then(response => response.json())
+        .then(parsedJSON => parsedJSON.results.map(user => (
+            {
+                name: `${user.name.first} ${user.name.last}`,
+                username: `${user.login.username}`,
+                email: `${user.email}`,
+                location: `${user.location.street}, ${user.location.city}`
+            }
+        )))
+        .then(contacts => this.setState({
+            contacts,
+            isLoading: false
+        }))
+        .catch(error => console.log('parsing failed', error))
+    }
+
+    render() {
+        const {isLoading, contacts} = this.state;
+        return (
+            <div>
+                <header>
+                    <img src={image} />
+                    <h1>Fetching Data <button className="btn btn-sm btn-danger">Fetch now</button></h1>
+                </header>
+                <div className={`content ${isLoading ? 'is-loading' : ''}`}>
+                    <div className="panel-group">
+                    {
+                        !isLoading && contacts.length > 0 ? contacts.map(contact => {
+                            const {username, email, name, location} = contact; 
+                            return <Collapsible key={username} title={name}>
+                            <p>{email}<br />{location}</p>
+                        </Collapsible>
+                        }) : null
+                    }
+                    </div>
+                    <div className="loader">
+                        <div className="icon"></div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default App;
